1efe0f1deabf347a087ff3b2094988c69808e58c
김명호 <audgh919@gmail.com>
2025-03-20 20:30:24 +0900

    코딩테스트연습: 완전탐색 카펫

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,28 @@
+package org.example;
+
+class Solution {
+
+    public int[] solution(int brown, int yellow) {
+        // 타일의 총 개수 구하기
+        int total = brown + yellow;
+
+        for (int width = total; width>=1; width--) {
+            if (total % width == 0) { // 총갯수/길이 나누어떨어질때
+                int height = total / width; // 높이
+
+                if ( (width-2) * (height - 2) == yellow ) {
+                    return new int[] {width, height};
+                }
+            }
+        }
+        return new int[]{0,0};
+    }
+}
+
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+    }
+}
